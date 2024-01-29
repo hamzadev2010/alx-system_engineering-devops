7240a822e7c640b55ced1c51161b1c7e76be710e
hamza chankour <hamzachanik@outlook.com>
2024-01-29 06:17:32 -0800

    task

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,20 +9,20 @@ import sys
 
 
 def fetch_data(id):
-    rq = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-    .format(id))
-    usr = rq.json()
+     rq = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+             .format(id))
+     usr = rq.json()
 
-    rq = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos"
-    .format(id))
-    todos = rq.json()
+     rq = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos"
+             .format(id))
+     todos = rq.json()
 
-    filename = "{}.csv".format(usr["id"])
-    with open(filename, "w") as file:
-        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-        for todo in todos:
-            writer.writerow([usr["id"], usr["username"],
-                            todo["completed"], todo["title"]])
+     filename = "{}.csv".format(usr["id"])
+     with open(filename, "w") as file:
+         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+         for todo in todos:
+             writer.writerow([usr["id"], usr["username"],
+                             todo["completed"], todo["title"]])
 
 
 if __name__ == "__main__":
