82c015e2a57e0661c312a5761c5eaedc6c588f51
hamza chankour <hamzachanik@outlook.com>
2024-01-29 07:15:13 -0800

    task

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Using what you did in the task #0,
+extend your Python script to export data in the JSON format."""
+
+import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    usrs = requests.get("https://jsonplaceholder.typicode.com/users")
+    usrs = usr.json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todos = todos.json()
+    todoAll = {}
+
+    for usr in usrs:
+        taskList = []
+        for task in todos:
+            if task.get('userId') == usr.get('id'):
+                taskDict = {"username": usr.get('username'),
+                            "task": task.get('title'),
+                            "completed": task.get('completed')}
+                taskList.append(taskDict)
+        todoAll[usr.get('id')] = taskList
+
+    with open('todo_all_employees.json', mode='w') as f:
+        json.dump(todoAll, f)
