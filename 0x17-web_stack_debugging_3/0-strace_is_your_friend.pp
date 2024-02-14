14b41e54671e12f140dcdded063c86c72ebc7af6
hamza chankour <hamzachanik@outlook.com>
2024-02-14 04:52:02 -0800

    task

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,5 +2,5 @@
 # Once you find the issue, fix it and then automate it using Puppet
 exec { 'fix-wordpress':
   command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-  path    => '/bin', '/usr/bin', '/usr/sbin',
+  path    => ['/bin', '/usr/bin', '/usr/sbin'],
 }
